6208b20f8794e74ad8e71d1e3dcd49f615448146
Alex Alexeyev <aasmc@yandex.ru>
2021-11-22 23:40:13 +0300

    Add skeleton MainActivity

--- app/src/main/java/ru/aasmc/jetnotes/MainActivity.kt
+++ app/src/main/java/ru/aasmc/jetnotes/MainActivity.kt
@@ -3,36 +3,33 @@ package ru.aasmc.jetnotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import ru.aasmc.jetnotes.theme.JetNotesTheme
+import ru.aasmc.jetnotes.viewmodel.MainViewModel
+import ru.aasmc.jetnotes.viewmodel.MainViewModelFactory
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels(
+        factoryProducer = {
+            MainViewModelFactory(
+                this,
+                (application as JetNotesApplication).dependencyInjector.repository
+            )
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            JetNotesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetNotesTheme {
-        Greeting("Android")
     }
 }
