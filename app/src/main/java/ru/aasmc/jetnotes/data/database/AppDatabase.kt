3722345dd1038a8bb3eaf935630270161ce55ea4
Alex Alexeyev <aasmc@yandex.ru>
2021-11-22 22:52:27 +0300

    Add AppDatabase

--- app/src/main/java/ru/aasmc/jetnotes/data/database/AppDatabase.kt
+++ app/src/main/java/ru/aasmc/jetnotes/data/database/AppDatabase.kt
@@ -1,4 +1,19 @@
 package ru.aasmc.jetnotes.data.database
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.aasmc.jetnotes.data.database.dao.ColorDao
+import ru.aasmc.jetnotes.data.database.dao.NoteDao
+import ru.aasmc.jetnotes.data.database.model.ColorDbModel
+import ru.aasmc.jetnotes.data.database.model.NoteDbModel
+
+@Database(entities = [NoteDbModel::class, ColorDbModel::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    companion object {
+        const val DATABASE_NAME = "note_maker_database.db"
+    }
+
+    abstract fun noteDao(): NoteDao
+
+    abstract fun colorDao(): ColorDao
 }
