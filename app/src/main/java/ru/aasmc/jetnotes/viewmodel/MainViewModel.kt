0ebe23481ee06eccc70bf30b4e90d3207a1f4173
Alex Alexeyev <aasmc@yandex.ru>
2021-11-24 23:50:17 +0300

    Add logic to viewModel to handle notes in trash

--- app/src/main/java/ru/aasmc/jetnotes/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/aasmc/jetnotes/viewmodel/MainViewModel.kt
@@ -1,10 +1,12 @@
 package ru.aasmc.jetnotes.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import ru.aasmc.jetnotes.data.repository.Repository
 import ru.aasmc.jetnotes.domain.model.NoteModel
 
@@ -20,6 +22,14 @@ class MainViewModel(
         repository.getAllNotesNotInTrash()
     }
 
+    val notesInTrash by lazy {
+        repository.getAllNotesInTrash()
+    }
+
+    private var _selectedNotes = MutableLiveData<List<NoteModel>>(listOf())
+    val selectedNotes: LiveData<List<NoteModel>>
+        get() = _selectedNotes
+
     //<editor-fold desc="Events that the view can pass">
     fun onCreateNewNoteClick() {
         // TODO
@@ -32,6 +42,34 @@ class MainViewModel(
     fun onNoteCheckedChange(note: NoteModel) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.insertNote(note)
+        }
+    }
+
+    fun onNoteSelected(note: NoteModel) {
+        _selectedNotes.value = _selectedNotes.value!!.toMutableList().apply {
+            if (contains(note)) {
+                remove(note)
+            } else {
+                add(note)
+            }
+        }
+    }
+
+    fun restoreNotes(notes: List<NoteModel>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.restoreNotesFromTrash(notes.map { it.id })
+            withContext(Dispatchers.Main) {
+                _selectedNotes.value = listOf()
+            }
+        }
+    }
+
+    fun permanentlyDeleteNotes(notes: List<NoteModel>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteNotes(notes.map { it.id })
+            withContext(Dispatchers.Main) {
+                _selectedNotes.value = listOf()
+            }
         }
     }
     //</editor-fold>
